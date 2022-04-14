4cd4c684ca10cf5dc708d4e03ac7afed505dc047
Corey DeLoach <cdeloach16@gmail.com>
2022-04-14 17:09:08 -0500

    initial commit, copied styles, setup trpc and prisma config

---
+++ apps/web/src/pages/index.tsx
@@ -0,0 +1,48 @@
+import Head from "next/head";
+import Image from "next/image";
+import Footer from "@/components/footer/footer";
+import Homepage from "@/components/homepage/homepage";
+import Navbar from "@/components/navbar/navbar";
+// import { trpc } from "@/utils/trpc";
+
+export default function Home() {
+  // const { data, isLoading } = trpc?.useQuery(["hello"]);
+  // if (isLoading) {
+  //   return <div>Loading...</div>;
+  // }
+  // if (data) {
+  //   return <div>{data.greeting}</div>;
+  // }
+  return (
+    <div>
+      <Head>
+        <title>Biotech | News</title>
+        <meta name="description" content="Spreading latest news in Biotech" />
+        {/* <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <link rel="manifest" href="/favicon/site.webmanifest" /> */}
+      </Head>
+      <Navbar />
+      <main>
+        <Homepage />
+      </main>
+      <Footer />
+    </div>
+  );
+}
